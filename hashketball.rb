43768bd16738f6002e6723ad6b132c1fa8599b20
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-19 01:46:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -229,3 +229,16 @@ def winning_team
   winners
 end
 
+def most_points_scored
+  most_points = 0
+  player_most_points_name = {}
+  game_hash.each{|home_away, teams_stat|
+    teams_stat[:players].each{|player|
+      if player[:points] > most_points
+        most_points = player[:points]
+        player_most_points_name = player[:player_name]
+      end
+    }
+  }
+  player_most_points_name
+end
